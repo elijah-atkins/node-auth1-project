7d437f95a96dba200c8c657205cd143286f027c5
Elijah Atkins <elijahorama@gmail.com>
2020-08-05 19:39:18 -0600

    mvp done will start on react app for stretch

--- api/server.js
+++ api/server.js
@@ -1,23 +1,52 @@
+const session = require('express-session');
+const knexSessionStore = require('connect-session-knex')(session);
+
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-//const session = require('express-sessions')
+const restricted = require('../middleware/restricted');
+const notfound = require('../middleware/404');
 
-//const usersRouter = require("../users/users-router.js");
-//const authRouter = require("../auth/auth-router")
+
+const usersRouter = require("../users/users-router.js");
+const authRouter = require("../auth/auth-router")
 
 const server = express();
+
+const sessionConfig = {
+    name: 'sksession',
+    secret: 'myspeshulsecret',
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false, // should be true in production
+      httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: false,
+  
+    store: new knexSessionStore(
+      {
+        knex: require("../database/configDB.js"),
+        tablename: "sessions",
+        sidfieldname: "sid",
+        createtable: true,
+        clearInterval: 1000 * 60 * 60
+      }
+    )
+  }
+
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-
-//server.use("/api/users", usersRouter);
-//server.use("/api/auth", authRouter);
+server.use(session(sessionConfig));
+server.use("/api/users", restricted, usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
+server.use(notfound)
 
 module.exports = server;
